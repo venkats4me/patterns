f1f4d607d510dfd1f58a79e8fb2d439a080babfb
vbasa <venkat2toc@gmail.com>
2025-04-23 12:34:01 +0530

    p3 code added

--- Patters/src/main/java/org/example/Pattern_3.java
+++ Patters/src/main/java/org/example/Pattern_3.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Pattern_3 {
     public static void main(String[] args) {
+    /*    *****
+          ****
+          ***
+          **
+          *
 
+     */
+        int r=5,c=5;
+
+        for (int i = 0; i < c; i++) {
+            for (int j = r; j >0; j--) {
+                System.out.print("*");
+            }
+            r--;
+            System.out.println();
+        }
+        System.out.println("=====logic 2======");
+        for (int i = c; i >=1 ; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

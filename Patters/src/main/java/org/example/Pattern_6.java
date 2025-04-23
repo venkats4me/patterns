e7b1ff565dfe9f0dcfd93c7db6ca564e938e44c5
vbasa <venkat2toc@gmail.com>
2025-04-23 17:58:48 +0530

    p code added

--- Patters/src/main/java/org/example/Pattern_6.java
+++ Patters/src/main/java/org/example/Pattern_6.java
@@ -10,12 +10,21 @@ public class Pattern_6 {
       *****
          */
         
-        int c=5,r=5;
-        for (int i = 0; i < ; i++) {
-            for (int j = 0; j < ; j++) {
-                
+        int c=5,r=5,ptc=0,ptr=4;
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < r; j++) {
+                System.out.print(ptc == i & ptr <= j ? "*" : " ");
             }
+            ptc++;
+            ptr--;
+            System.out.println();
         }
         
     }
 }
+/*
+ if(i>=ptc&&j>=ptr){
+        System.out.print("*");
+ptc++;
+ptr--;
+        }*/
